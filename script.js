d9bafb211732efc8cdba99e4fdb534daff78ce54
YASH CHAUDHARI <119956179+Yashchaudhari29@users.noreply.github.com>
2023-06-27 17:47:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.getElementById("btn").addEventListener('click', e => {
+    e.preventDefault()
+    nam = document.getElementById("name").value
+    textarea = document.getElementById("textarea").value
+    if (!nam || !textarea) {
+        alert("Please fillup the fields")
+        return 0
+    }
+
+    var form = document.getElementById("form")
+    form.action = "https://sheetdb.io/api/v1/i7e0yxqjhiwtx"
+
+    try {
+        fetch(form.action, {
+            method: 'POST',
+            body: new FormData(form)
+        }).then(
+           alert("Response Recorded")
+        )
+            .then(res => {
+                console.log(res)
+            })
+    }
+    catch (e) {
+        alert("Error:" + e)
+    }
+
+},
+
+)
+
