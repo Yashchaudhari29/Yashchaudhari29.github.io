06c756e33d999bfa1869f0714426c21a3aff62be
YASH CHAUDHARI <119956179+Yashchaudhari29@users.noreply.github.com>
2024-06-10 21:33:01 +0530

    Update download.js

--- script/download.js
+++ script/download.js
@@ -1,6 +1,6 @@
 document.querySelector(".btn").addEventListener("click", function () {
-  alert('Check CV on laptop for certifications links.')
-  var cvUrl = './Yash_cv.pdf';
+  // alert('Check CV on laptop for certifications links.')
+  var cvUrl = './Yash Chaudhari.pdf';
   var link = document.createElement("a");
   link.href = cvUrl;
   link.download = "Yash CV.pdf";
